a2e7947b67543d184251ed156656a2498261d93a
Leebm <maximilian.leeb02005@gmail.com>
2022-06-13 12:04:07 +0200

    first

---
+++ site/templates/basic-page.php
@@ -0,0 +1,16 @@
+<?php namespace ProcessWire; 
+
+// Template file for pages using the “basic-page” template
+
+?>
+
+
+<div id="content">
+	Basic page content 
+	<?php
+
+echo $page->slogan;
+
+?>
+</div>	
+
---
+++ site/templates/home.php
@@ -0,0 +1,13 @@
+<?php namespace ProcessWire;
+
+// Template file for “home” template used by the homepage
+
+?>
+
+<div id="content">
+	Homepage content 
+
+
+</div>	
+
+
